b4d73a1131f09c4fef9486e7528e6819670bb7ec
Faxe10 <faxe@punkmail.org>
2025-11-04 18:59:42 +0100

    small changes

---
+++ fpga/time_tagger/TimeTagger.srcs/sources_1/new/raw_to_time.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/04/2025 06:55:26 PM
+// Design Name: 
+// Module Name: raw_to_time
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+`include "settings.vh"
+module raw_to_time(
+input wire [ $clog2(`Channel_num)-1:0] iChannel,
+
+    );
+endmodule
--- fpga/time_tagger/TimeTagger.srcs/sources_1/new/settings.vh
+++ fpga/time_tagger/TimeTagger.srcs/sources_1/new/settings.vh
@@ -1,5 +1,7 @@
 `ifndef SETTINGS_VH
 `define SETTINGS_VH
     `define NUM_TAPPS 400
-    `define WIDHT_HISTOGRAM 8
+    `define WIDHT_HISTOGRAM 14
+    `define Counts_for_cal 750000
+    `define Channel_num 8
 `endif
