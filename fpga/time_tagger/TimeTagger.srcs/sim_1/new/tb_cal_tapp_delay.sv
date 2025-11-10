140cdf6890a38d6eb0e340f2533646356d5ed35c
Faxe10 <faxe@punkmail.org>
2025-11-10 18:56:13 +0100

    tb_cal_tapp_delay works

--- fpga/time_tagger/TimeTagger.srcs/sim_1/new/tb_cal_tapp_delay.sv
+++ fpga/time_tagger/TimeTagger.srcs/sim_1/new/tb_cal_tapp_delay.sv
@@ -1,13 +1,13 @@
-`timescale 1us / 1ns
+`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: Light and Matter Group, Leibniz University Hannover
-// Engineer: Fabian Walther fabian@cryptix.de
+// Company: 
+// Engineer: 
 // 
-// Create Date: 11/10/2025 03:07:19 PM
-// Design Name: Photonen Coincidence Counter
-// Module Name: tb_hits_per_tapp
-// Project Name: 2QA Entanglement demonstrator
-// Target Devices: EBAZ4205
+// Create Date: 11/10/2025 05:56:47 PM
+// Design Name: 
+// Module Name: tb_cal_tapp_delay
+// Project Name: 
+// Target Devices: 
 // Tool Versions: 
 // Description: 
 // 
@@ -18,17 +18,19 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 `default_nettype none
 `include "settings.vh"
-module tb_hits_per_tapp(
+
+module tb_cal_tapp_delay(
 
     );
-
+    
   //clock stuff
   logic clk;
   initial clk = 0;
   always #5 clk = ~clk; // 100 MHz
-  // port stuff
+  // port  hits_per_tapp stuff
   logic reset;
   logic new_stop_val_w;
   logic [$clog2(`NUM_TAPPS)-1:0]tapp_stop_val_w;
@@ -37,11 +39,16 @@ module tb_hits_per_tapp(
   logic [$clog2(`NUM_TAPPS)-1:0] read_tapp_addr_w;
   logic  [`WIDTH_HISTOGRAM-1:0]tapp_counts_w;
   logic [32:0]counts_total_w;
-  
+  //port cal tapp delay stuff
+  logic write_new_delay_w;
+  logic [$clog2(`MAX_FINE_VAL)-1:0]  new_tapp_delay_w;
+  logic [$clog2(`NUM_TAPPS)-1:0] write_tapp_addr_w;
   
   // test stuff
   logic[`WIDTH_HISTOGRAM-1:0] read_counts;
   logic [32:0]counts_total_r;
+  logic [32:0]x; 
+  logic [$clog2(`MAX_FINE_VAL)-1:0]  random;
   hits_per_tapp hits_per_tapp_inst(
         .iCLK(clk),
         .iRST(reset),
@@ -55,6 +62,26 @@ module tb_hits_per_tapp(
         .oRd_data(tapp_counts_w),
         .oTotal(counts_total_w)
     );
+     cal_tapp_delay cal_tapp_delay_inst(
+        .iCLK(clk),
+        .iRST(reset),
+        // com hist per tapp
+        .iTapp_counts(tapp_counts_w),
+        .iTotal_counts(counts_total_w),
+        .oRead_Tapp_Addr(read_tapp_addr_w),
+        .oRead_Tapp(read_tapp_w),
+        .oStop_Counting(stop_counting_w),
+        // output new delay val
+        .oTapp_delay(new_tapp_delay_w),
+        .oTapp_num( write_tapp_addr_w),
+        .oWrite_new_delay(write_new_delay_w)
+    );
+    gen_time_tag gen_time_tag_inst(
+        .iCLK(clk),
+        .iWrite_new_delay(write_new_delay_w),
+        .iTapp_delay(new_tapp_delay_w),
+        .iWrite_tapp_addr(write_tapp_addr_w)
+    );
     task automatic hit( input int addr);
         begin
             @(posedge clk);
@@ -65,17 +92,7 @@ module tb_hits_per_tapp(
             repeat (4) @(posedge clk);
         end
     endtask
-    task automatic  read(input int addr);
-        begin
-            read_tapp_addr_w <= addr;
-            read_tapp_w <= 1'b1;
-            @(posedge clk);
-            read_tapp_w <= 1'b0;
-            @(posedge clk);
-            read_counts <= tapp_counts_w;
-            @(posedge clk);
-        end
-    endtask 
+
     task automatic reset_task ();
         begin 
             reset <= 1'b1;
@@ -84,38 +101,12 @@ module tb_hits_per_tapp(
         end
     endtask
     initial begin
-        stop_counting_w <= 1'b0;
         reset_task;
-        repeat (400)@(posedge clk);
-        hit(5);
-        read(5);
-        if (read_counts != 1)begin 
-              $display("error counts false");
+        for (x = 0; x <= `COUNTS_FOR_CAL+1;x++)begin
+            random = $urandom_range(0, 400);
+            hit(random);
+            @(posedge clk);
+            $display("test");        
         end
-        hit(100);
-        hit(100);
-        hit(100);
-        read(100);
-        if (read_counts != 3)begin 
-              $display("error counts false");
-        end   
-        hit(101);
-        if (read_counts != 3)begin 
-              $display("error counts false");
-        end  
-        hit(100);
-        hit(101);
-        hit(100);
-        read(100);
-        if (read_counts != 5)begin 
-              $display("error counts false");
-        end  
-        counts_total_r <= counts_total_w;
-        reset_task;
-        repeat(400) @(posedge clk); 
-        read(100);
-        if (read_counts != 0)begin 
-              $display("error reset dosent work");
-        end  
     end
 endmodule
